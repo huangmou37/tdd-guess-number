1288932890b9fdcdf280b7b73ad9254fdd035944
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-06-22 18:03:38 +0800

    [Cheng] Test match guess given input with 1 correct and 2 wrong position

--- src/main/java/cn/tdd/guessnumber/GuessMatcher.java
+++ src/main/java/cn/tdd/guessnumber/GuessMatcher.java
@@ -3,9 +3,25 @@ package cn.tdd.guessnumber;
 public class GuessMatcher {
 
   public GuessMatcher(String answer) {
+    this.answer = answer;
   }
 
   public String matchGuess(String guess) {
-    return "4A0B";
+    int numberOfCorrect = 0;
+    int numberOfWrongPosition = 0;
+
+    for (int i = 0; i < guess.length(); i ++) {
+      char guessNum = guess.charAt(i);
+
+      if (guessNum == answer.charAt(i)) {
+        numberOfCorrect ++;
+      } else if (answer.indexOf(guessNum) > -1) {
+        numberOfWrongPosition ++;
+      }
+    }
+
+    return String.format("%dA%dB", numberOfCorrect, numberOfWrongPosition);
   }
+
+  private String answer;
 }
--- src/test/java/cn/tdd/guessnumber/GuessMatcherTest.java
+++ src/test/java/cn/tdd/guessnumber/GuessMatcherTest.java
@@ -12,4 +12,11 @@ public class GuessMatcherTest {
 
     assertEquals("4A0B", guessMatcher.matchGuess("1234"));
   }
+
+  @Test
+  public void should_return_1A2B_when_match_guess_given_input_with_1_correct_and_2_wrong_position() {
+    GuessMatcher guessMatcher = new GuessMatcher("1234");
+
+    assertEquals("1A2B", guessMatcher.matchGuess("0324"));
+  }
 }
