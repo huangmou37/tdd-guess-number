4d0b302d6e8aac067ae4699c8cc1a66e643f3b68
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-06-22 20:23:44 +0800

    [Cheng] Test for running game with six wrong guess

--- src/main/java/cn/tdd/guessnumber/GuessNumberGame.java
+++ src/main/java/cn/tdd/guessnumber/GuessNumberGame.java
@@ -1,5 +1,10 @@
 package cn.tdd.guessnumber;
 
+import cn.tdd.guessnumber.exception.WrongInputException;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GuessNumberGame {
@@ -7,6 +12,7 @@ public class GuessNumberGame {
   public void start() {
     String answer = answerGenerator.generate();
     guessMatcher = new GuessMatcher(answer);
+    guessHistory = new ArrayList<>();
     boolean isGameOver = false;
 
     Scanner scanner = new Scanner(System.in);
@@ -14,17 +20,40 @@ public class GuessNumberGame {
       String userInput = scanner.nextLine();
 
       String guessMatchResult;
-      guessMatchResult = guessMatcher.matchGuess(userInput);
+      try {
+        guessMatchResult = guessMatcher.matchGuess(userInput);
+      } catch (WrongInputException e) {
+        guessMatchResult = WRONG_INPUT;
+      }
+
+      guessHistory.add(new SimpleEntry<>(userInput, guessMatchResult));
 
       if (guessMatchResult.equals("4A0B")) {
         System.out.println(userInput + " -> " + guessMatchResult);
         System.out.println("Correct!");
         isGameOver = true;
+      } else if (guessHistory.size() == RETRY_TIMES) {
+        System.out.println(userInput + " -> " + guessMatchResult);
+        System.out.println("Sorry, game over!");
+        isGameOver = true;
+      }
+
+      if (!isGameOver) {
+        for (SimpleEntry<String, String> historyEntry: guessHistory) {
+          System.out.println(historyEntry.getKey() + " -> " + historyEntry.getValue());
+        }
+        System.out.println("Please try again:");
       }
     } while (!isGameOver);
   }
 
+  private static final String WRONG_INPUT = "Wrong Input，Input again";
+
+  private static final int RETRY_TIMES = 6;
+
   private RandomAnswerGenerator answerGenerator = new RandomAnswerGenerator();
 
   private GuessMatcher guessMatcher;
+
+  private List<SimpleEntry<String, String>> guessHistory;
 }
--- src/test/java/cn/tdd/guessnumber/GuessNumberGameTest.java
+++ src/test/java/cn/tdd/guessnumber/GuessNumberGameTest.java
@@ -40,4 +40,36 @@ public class GuessNumberGameTest {
 
     assertEquals("1234 -> 4A0B\nCorrect!\n", outputStream.toString());
   }
+
+  @Test
+  public void should_end_game_and_print_guess_history_when_running_game_given_6_wrong_guess() {
+    ByteArrayInputStream in = new ByteArrayInputStream("abcd\n2345\n3456\n4567\n5678\n6789\n".getBytes());
+    System.setIn(in);
+
+    guessNumberGame.start();
+
+    assertEquals(
+        "abcd -> Wrong Input，Input again\n"
+            + "Please try again:\n"
+            + "abcd -> Wrong Input，Input again\n"
+            + "2345 -> 0A3B\n"
+            + "Please try again:\n"
+            + "abcd -> Wrong Input，Input again\n"
+            + "2345 -> 0A3B\n"
+            + "3456 -> 0A2B\n"
+            + "Please try again:\n"
+            + "abcd -> Wrong Input，Input again\n"
+            + "2345 -> 0A3B\n"
+            + "3456 -> 0A2B\n"
+            + "4567 -> 0A1B\n"
+            + "Please try again:\n"
+            + "abcd -> Wrong Input，Input again\n"
+            + "2345 -> 0A3B\n"
+            + "3456 -> 0A2B\n"
+            + "4567 -> 0A1B\n"
+            + "5678 -> 0A0B\n"
+            + "Please try again:\n"
+            + "6789 -> 0A0B\n"
+            + "Sorry, game over!\n", outputStream.toString());
+  }
 }
