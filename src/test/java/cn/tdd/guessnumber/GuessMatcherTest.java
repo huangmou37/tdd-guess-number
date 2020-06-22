bffcedf3daa8c3c08a83a705f88c539712e7b1fe
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-06-22 18:07:31 +0800

    [Cheng] Test match guess given input with all wrong

--- src/test/java/cn/tdd/guessnumber/GuessMatcherTest.java
+++ src/test/java/cn/tdd/guessnumber/GuessMatcherTest.java
@@ -1,22 +1,37 @@
 package cn.tdd.guessnumber;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class GuessMatcherTest {
 
+  private GuessMatcher guessMatcher;
+
+  @Before
+  public void init() {
+    this.guessMatcher = new GuessMatcher("1234");
+  }
+
   @Test
   public void should_return_4A0B_when_match_guess_given_input_with_all_correct() {
-    GuessMatcher guessMatcher = new GuessMatcher("1234");
+    String matchResult = guessMatcher.matchGuess("1234");
 
-    assertEquals("4A0B", guessMatcher.matchGuess("1234"));
+    assertEquals("4A0B", matchResult);
   }
 
   @Test
   public void should_return_1A2B_when_match_guess_given_input_with_1_correct_and_2_wrong_position() {
-    GuessMatcher guessMatcher = new GuessMatcher("1234");
+    String matchResult = guessMatcher.matchGuess("0324");
 
-    assertEquals("1A2B", guessMatcher.matchGuess("0324"));
+    assertEquals("1A2B", matchResult);
+  }
+
+  @Test
+  public void should_return_0A0B_when_match_guess_given_input_with_all_wrong() {
+    String matchResult = guessMatcher.matchGuess("5678");
+
+    assertEquals("0A0B", matchResult);
   }
 }
